a7efd64837a97ff625ed221352638340fb51c1de
Brad Garropy <bgarropy@atlassian.com>
2023-04-25 21:35:46 -0500

    new components. fix styling issues.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
+            aspectRatio: {
+                thumbnail: "1.5",
+            },
             fontFamily: {
                 sans: ["Cabin", ...defaultTheme.fontFamily.sans],
             },
