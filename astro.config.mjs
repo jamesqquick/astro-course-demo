5a23e6f48e330312ddb5be1ed239da20114c3f0e
Brad Garropy <bgarropy@atlassian.com>
2023-05-01 14:14:45 -0500

    progress.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import {defineConfig} from "astro/config"
 import tailwind from "@astrojs/tailwind"
-
 import image from "@astrojs/image"
+
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), image()],
+    integrations: [tailwind(), image(), svelte()],
 })
