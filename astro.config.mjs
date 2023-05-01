5ab4de604e11a7205cb8b9d8288f103b04faa4aa
Brad Garropy <bgarropy@atlassian.com>
2023-05-01 14:55:20 -0500

    remove svelte.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import {defineConfig} from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import image from "@astrojs/image"
 
-import svelte from "@astrojs/svelte"
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), image(), svelte()],
+    integrations: [tailwind(), image()],
 })
