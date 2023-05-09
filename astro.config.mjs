40450e37000ae1756cc2551fe89b24fc68172944
Brad Garropy <bgarropy@atlassian.com>
2023-05-09 10:31:30 -0500

    add sitemap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import {defineConfig} from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import image from "@astrojs/image"
-
 import vercel from "@astrojs/vercel/serverless"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), image()],
-    output: "server",
     adapter: vercel(),
+    integrations: [tailwind(), image(), sitemap()],
+    output: "server",
+    site: "https://rhythm.nation",
 })
