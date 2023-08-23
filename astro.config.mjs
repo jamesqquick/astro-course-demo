ec3de60ccc7b42e95b2c6bba60c683980b22e6e4
jamesqquick <james.q.quick@gmail.com>
2023-08-23 15:57:46 -0500

    Successful upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import {defineConfig} from "astro/config"
 import tailwind from "@astrojs/tailwind"
-import image from "@astrojs/image"
 import vercel from "@astrojs/vercel/serverless"
 import sitemap from "@astrojs/sitemap"
 import {remarkReadingTime} from "./src/remarkReadingTime"
@@ -8,10 +7,13 @@ import {remarkReadingTime} from "./src/remarkReadingTime"
 // https://astro.build/config
 export default defineConfig({
     adapter: vercel(),
-    integrations: [tailwind(), image(), sitemap()],
+    integrations: [tailwind(), sitemap()],
     markdown: {
         remarkPlugins: [remarkReadingTime],
     },
     output: "server",
     site: "https://rhythm.nation",
+    server: {
+        port: 3000,
+    },
 })
