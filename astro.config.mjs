4eea37067f2e393a0dfe161690fa0086ea30724f
jamesqquick <james.q.quick@gmail.com>
2023-09-10 08:49:04 -0500

    Ready to deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import {defineConfig} from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
 import {remarkReadingTime} from "./src/remarkReadingTime"
-import vercel from "@astrojs/vercel/serverless"
+import netlify from "@astrojs/netlify/functions"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
         remarkPlugins: [remarkReadingTime],
     },
     output: "server",
-    adapter: vercel(),
+    adapter: netlify(),
     site: "https://rhythm.nation",
     server: {
         port: 3000,
