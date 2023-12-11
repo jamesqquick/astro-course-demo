7b93902d40e1d82fa5649ff20bc78cbd4b143b16
jamesqquick <james.q.quick@gmail.com>
2023-12-11 15:07:41 -0600

    Added contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,22 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
-import netlify from "@astrojs/netlify/functions";
-import sitemap from "@astrojs/sitemap";
-import { remarkReadingTime } from "./src/remarkReadingTime";
+import {defineConfig} from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/static"
+import netlify from "@astrojs/netlify/functions"
+import sitemap from "@astrojs/sitemap"
+import {remarkReadingTime} from "./src/remarkReadingTime"
 
-import svelte from "@astrojs/svelte";
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
-  integrations: [tailwind(), sitemap(), svelte()],
-  markdown: {
-    remarkPlugins: [remarkReadingTime]
-  },
-  output: "static",
-  site: "https://rhythm.nation",
-  server: {
-    port: 3000
-  }
-});
+    adapter: vercel(),
+    integrations: [tailwind(), sitemap(), svelte()],
+    markdown: {
+        remarkPlugins: [remarkReadingTime],
+    },
+    output: "static",
+    site: "https://rhythm.nation",
+    server: {
+        port: 3000,
+    },
+})
--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
     import kwesforms from "kwesforms"
     import {onMount} from "svelte"
+
     onMount(() => {
         kwesforms.init()
     })
 </script>
 
 <form
-    method="POST"
-    action="https://kwesforms.com/api/foreign/forms/U72jfYsoZ9VI1KsugjYN"
-    class="kwes-form grid gap-y-2 items-end max-md:grid-cols-1"
-    redirect="/thankyou"
+    class="kwes-form grid gap-y-2"
+    action="https://kwesforms.com/api/foreign/forms/gyxHlsYWTE0Wjzi1fxOe"
     no-error-message
+    redirect="/thankyou"
 >
     <label for="comment" class="text-zinc-900 text-2xl mb-2 mt-10">
         What's on your mind?
@@ -21,10 +21,9 @@
         rows="3"
         name="comment"
         id="comment"
-        class="border border-teal-900 rounded-lg text-2xl px-6 py-6"
         rules="required|max:255"
+        class="border border-teal-900 rounded-lg text-2xl px-6 py-6"
     />
-
     <button
         type="submit"
         class="text-2xl text-zinc-100 bg-teal-900 px-6 py-5 rounded-xl my-1"
