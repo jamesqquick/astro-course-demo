c2c694ec249ed50adb67d06a4682dd3916c8b3e4
jamesqquick <james.q.quick@gmail.com>
2023-12-12 14:20:46 -0600

    Added sentry spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,27 @@ import vercel from "@astrojs/vercel/static"
 import netlify from "@astrojs/netlify/functions"
 import sitemap from "@astrojs/sitemap"
 import {remarkReadingTime} from "./src/remarkReadingTime"
-
 import svelte from "@astrojs/svelte"
+
+import sentry from "@sentry/astro"
+import spotlightjs from "@spotlightjs/astro"
 
 // https://astro.build/config
 export default defineConfig({
     adapter: vercel(),
-    integrations: [tailwind(), sitemap(), svelte()],
+    integrations: [
+        tailwind(),
+        sitemap(),
+        svelte(),
+        sentry({
+            dsn: process.env.SENTRY_DSN,
+            sourceMapsUploadOptions: {
+                project: process.env.SENTRY_PROJECT_NAME,
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+            },
+        }),
+        spotlightjs(),
+    ],
     markdown: {
         remarkPlugins: [remarkReadingTime],
     },
