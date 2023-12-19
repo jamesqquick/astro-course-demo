9ff0dc51b497a1323414e35891a0232cf1c97b5c
jamesqquick <james.q.quick@gmail.com>
2023-12-19 14:51:53 -0600

    Added sentry and spotlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,25 @@
-import {defineConfig} from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import sitemap from "@astrojs/sitemap"
-import {remarkReadingTime} from "./src/remarkReadingTime"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import { remarkReadingTime } from "./src/remarkReadingTime";
 // import vercel from "@astrojs/vercel/serverless"
-import netlify from "@astrojs/netlify/functions"
-import svelte from "@astrojs/svelte"
+import netlify from "@astrojs/netlify/functions";
+import svelte from "@astrojs/svelte";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), sitemap(), svelte()],
-    markdown: {
-        remarkPlugins: [remarkReadingTime],
-    },
-    output: "server",
-    // adapter: vercel(),
-    adapter: netlify(),
-    site: "https://rhythm.nation",
-    server: {
-        port: 3000,
-    },
-})
+  integrations: [tailwind(), sitemap(), svelte(), sentry(), spotlightjs()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime]
+  },
+  output: "server",
+  // adapter: vercel(),
+  adapter: netlify(),
+  site: "https://rhythm.nation",
+  server: {
+    port: 3000
+  }
+});
